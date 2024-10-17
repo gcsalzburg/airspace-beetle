0dc0765d099caaf833beed3760aec3f24963d13f
George Cave <george@interactionmagic.com>
2024-10-17 23:11:53 +0100

    add bw option

--- include/scripts.js
+++ include/scripts.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 		mapbox_token: 'pk.eyJ1IjoiZ2NzYWx6YnVyZyIsImEiOiJjam1pNm5uZmcwMXNyM3FtNGp6dTY3MGxsIn0.PmLPkI3T8UxjEIPnz7fxEA',
 		mapbox_style: 'mapbox://styles/annamitch/clsded3i901rg01qyc16p8dzw',
+	//	mapbox_syle: 'mapbox://styles/mapbox/light-monochrome',
 
 		dom: {
 			mapbox: document.querySelector('.map'),
